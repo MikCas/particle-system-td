8a6cb2c7b925c429b8c0dd2816c5113afa354bb1
MikCas <mikhail.cassar@gmail.com>
2026-03-22 22:32:50 +0100

    Updated Particle Parameters - using Stateful/Derived parameters. Added Seed parameter to Particle struct, and STATEFUL node. Intending to remove the STATIC/DYNAMIC particle split

--- src/glsl/particle.glsl
+++ src/glsl/particle.glsl
@@ -1,19 +1,14 @@
-struct Particle{	
-	// --- IDENTITY ---
-	uint id;
+// Particle data structure and I/O
 
-	// --- DYNAMIC ---
-	// Attributes which are updated during life
-	vec3 pos;
-	vec3 vel;
-	vec3 dir;
-	float age;
+struct Particle{	
+	uint id;
+	vec3 pos, vel;
+	float age, life;
+    float seed;
+
+    vec3 dir;
 	vec3 size;
 	vec4 color;
-
-	// --- STATIC ---
-	// Attrbiutes which are constant during life
-	float life;
 	float mass;
 	vec3 baseSize;
 	vec4 startColor;
@@ -24,15 +19,13 @@ struct Particle{
 Particle ReadParticle(in uint id) {
     Particle p;
     p.id = id;
-    
-	// --- DYNAMIC ---
     p.pos = TDIn_P(0, id);
     p.vel = TDIn_Vel(0, id);
-    p.dir = TDIn_Dir(0, id);
     p.age = TDIn_Age(0, id);
-
-	// --- STATIC ---
     p.life = TDIn_Life(0, id);
+    p.seed = TDIn_Seed(0, id);
+
+    p.dir = TDIn_Dir(0, id);
     p.mass = TDIn_Mass(0, id);
     p.baseSize = TDIn_BaseSize(0, id);  
     p.startColor = TDIn_StartColor(0, id);
@@ -44,16 +37,15 @@ Particle ReadParticle(in uint id) {
 // Write particle data to glsl pop
 void WriteParticle(in Particle p) {
     
-    // --- DYNAMIC ---
     P[p.id] = p.pos;
     Vel[p.id] = p.vel;
-    Dir[p.id] = p.dir;
     Age[p.id] = p.age;
+    Life[p.id] = p.life;
+    Seed[p.id] = p.seed;
     Size[p.id] = p.size;
     Color[p.id] = p.color;
 
-    // --- STATIC ---
-    Life[p.id] = p.life;
+    Dir[p.id] = p.dir;
     Mass[p.id] = p.mass;
     BaseSize[p.id] = p.baseSize;
     StartColor[p.id] = p.startColor;
